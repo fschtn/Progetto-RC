bef4e58a72a1975574ef677a95b8db3df74a6382
francescosche <schettino.francesco@live.it>
2018-02-24 11:40:19 +0100

    Accesso Google

--- files/js/script.js
+++ files/js/script.js
@@ -1,11 +1,8 @@
-// Funzioni da realizzare:
-//
-//     pageLoad()
-//     richiamata al caricamento della pagina, ottiene nome dell'utente attualmente loggato (tramite getUserName()) e la sua immagine del profilo (tramite getUserImage()), e le mostra all'interno del menu
-//
-//
-//
-//     checkLogin()
+function pageLoad() {
+//     richiamata al caricamento della pagina, ottiene nome dell'utente attualmente loggato (tramite getUserName()) e la sua immagine del profilo (tramite getUserImage()), e le mostra all'interno del menu. Richiama poi checkLogin()
+}
+
+function checkLogin() {
 //     Se ci si trova in "index.html"
 //         Se sono stati eseguiti entrambi gli accessi
 //             Reindirizza l'utente a "profile.html"
@@ -14,39 +11,36 @@
 //     Altrimenti
 //         Se NON sono stati eseguiti entrambi gli accessi
 //             Reindirizza l'utente a "index"
-//
-//
-//
-//     getUserName()
+}
+
+function getUserName() {
 //     ottiene l'id dell'utente attualmente connesso e, poi,
 //     tramite le API, il Nome dell'utente corrispondente, e lo restituisce
-//
-//     getUserImage()
+}
+
+function getUserImage() {
 //     ottiene l'id dell'utente attualmente connesso e, poi,
 //     tramite le API, l'URL dell'immagine del profilo dell'utente corrispondente, e lo restituisce
-//
-//
-//
-//     addToLibrary(id, library)
+}
+
+function addToLibrary(id, library) {
 //         id = id del libro
 //         library = [past,present,future]
 //     Aggiunge il libro avente <id> all'interno della libreria <library>
-//
-//
-//
-//     getBookInfo(id)
+}
+
+function getBookInfo(id) {
 //         id = id del libro
 //     Restituisce le informazioni del libro che ha <id>
-//
-//
-//
-//     getList(type, limit)
+}
+
+function getList(type, limit) {
 //         type = [past,present,future]
 //         limit = (1,100)
 //     Ottiene i primi <limit> libri della lista <type>
-//
-//
-//
-//     searchBook(title)
+}
+
+function searchBook(title) {
 //         title = stringa, titolo del libro da cercare
 //     Cerca, tramite le API di Google, il libro con titolo uguale a <title>
+}
